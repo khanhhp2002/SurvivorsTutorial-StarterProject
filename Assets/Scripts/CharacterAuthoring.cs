cd392f45148e4ff72caecea0015443e68406d3b7
khanhbq2002 <khanhbq2002@g5.studio>
2026-01-14 09:48:00 +0700

    [Update] character destruction

--- Assets/Scripts/CharacterAuthoring.cs
+++ Assets/Scripts/CharacterAuthoring.cs
@@ -23,6 +23,8 @@ public class CharacterAuthoring : MonoBehaviour
             AddComponent(entity, new CharacterMaxHitPoints { Value = authoring.MaxHitPoints });
             AddComponent(entity, new CharacterCurrentHitPoints { Value = authoring.MaxHitPoints });
             AddBuffer<DamageThisFrame>(entity);
+            AddComponent<DestroyEntityFlag>(entity);
+            SetComponentEnabled<DestroyEntityFlag>(entity, false);
         }
     }
 }
@@ -65,22 +67,22 @@ public partial struct CharacterMoveSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (moveDirection, moveSpeed, velocity, facingDirection,entity) in
+        foreach (var (moveDirection, moveSpeed, velocity, facingDirection, entity) in
                  SystemAPI.Query<CharacterMoveDirection, CharacterMoveSpeed, RefRW<PhysicsVelocity>, RefRW<FacingDirectionOverride>>()
                  .WithEntityAccess())
         {
             float2 moveStep2d = moveDirection.Value * moveSpeed.Value;
             velocity.ValueRW.Linear = new float3(moveStep2d, 0f);
 
-            if(math.abs(moveStep2d.x) > 0.15f)
+            if (math.abs(moveStep2d.x) > 0.15f)
             {
                 facingDirection.ValueRW.Value = math.sign(moveStep2d.x);
             }
 
-            if(SystemAPI.HasComponent<PlayerTag>(entity))
+            if (SystemAPI.HasComponent<PlayerTag>(entity))
             {
-               var animationOverride = SystemAPI.GetComponentRW<AnimationIndexOverride>(entity);
-               animationOverride.ValueRW.Value = math.lengthsq(moveStep2d) > float.Epsilon ? (float)PlayerAnimationIndex.Movement : (float)PlayerAnimationIndex.Idle;
+                var animationOverride = SystemAPI.GetComponentRW<AnimationIndexOverride>(entity);
+                animationOverride.ValueRW.Value = math.lengthsq(moveStep2d) > float.Epsilon ? (float)PlayerAnimationIndex.Movement : (float)PlayerAnimationIndex.Idle;
             }
         }
     }
@@ -107,12 +109,14 @@ public partial struct ProcessDamageThisFrameSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (currentHitPoints, damageBuffer) in
-                 SystemAPI.Query<RefRW<CharacterCurrentHitPoints>, DynamicBuffer<DamageThisFrame>>())
+        foreach (var (currentHitPoints, damageBuffer, entity) in
+                 SystemAPI.Query<RefRW<CharacterCurrentHitPoints>, DynamicBuffer<DamageThisFrame>>()
+                 .WithPresent<DestroyEntityFlag>() // Has to have DestroyEntityFlag component but ignore whether it's enabled or not
+                 .WithEntityAccess())
         {
-            if(damageBuffer.IsEmpty)
+            if (damageBuffer.IsEmpty)
                 continue;
-                
+
             float totalDamage = 0f;
             for (int i = 0; i < damageBuffer.Length; i++)
             {
@@ -121,6 +125,11 @@ public partial struct ProcessDamageThisFrameSystem : ISystem
 
             currentHitPoints.ValueRW.Value -= totalDamage;
             damageBuffer.Clear();
+
+            if (currentHitPoints.ValueRO.Value <= 0)
+            {
+                SystemAPI.SetComponentEnabled<DestroyEntityFlag>(entity, true);
+            }
         }
     }
 }
---
+++ Assets/Scripts/DestroyEntitySystem.cs
@@ -0,0 +1,40 @@
+using TMG.Survivors;
+using Unity.Burst;
+using Unity.Entities;
+
+[UpdateInGroup(typeof(SimulationSystemGroup), OrderLast = true)]
+[UpdateBefore(typeof(EndSimulationEntityCommandBufferSystem))]
+public partial struct DestroyEntitySystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var endEcb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = endEcb.CreateCommandBuffer(state.WorldUnmanaged);
+
+        foreach (var (_, entity) in
+                 SystemAPI.Query<DestroyEntityFlag>()
+                 .WithEntityAccess())
+        {
+            if(SystemAPI.HasComponent<PlayerTag>(entity))
+            {
+                GameUIController.Instance.ShowGameOverUI();
+            }
+            ecb.DestroyEntity(entity);
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+
+    }
+}
+
+public struct DestroyEntityFlag : IComponentData, IEnableableComponent{}
